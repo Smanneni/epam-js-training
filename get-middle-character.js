c543a3a4ed49547f104040410555c38103c5a345
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-04-26 09:57:33 +0530

    updated with review comments

--- get-middle-character.js
+++ get-middle-character.js
@@ -1,9 +1,10 @@
 function getMiddle(inputstring) {
-    Var middleChar;
-    if ((inputstring.length) % 2 == 0) {
-        middleChar = inputstring.charAt((inputstring.length / 2) - 1) + inputstring.charAt((inputstring.length / 2))
+    var middleChar, stringLength;
+    stringLength = (inputstring.length);
+    if (stringLength % 2 == 0) {
+        middleChar = inputstring.charAt((stringLength / 2) - 1) + inputstring.charAt((stringLength / 2))
     } else {
-        middleChar = inputstring.charAt(Math.floor(inputstring.length / 2))
+        middleChar = inputstring.charAt(Math.floor(stringLength / 2))
     }
 
     return middleChar;
