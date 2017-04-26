76fc63e2c15bf4c792a760d463271ed66a30cfaa
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-04-26 10:13:52 +0530

    updated with review comments

--- rental-car-cost.js
+++ rental-car-cost.js
@@ -1,23 +1,18 @@
 function rentalCarCost(Noofdays) {
-var DialyCarRent="$40";
-var RentDiscountperdays = {
-    days: [3,7],
-    discount: ["$20","$50"]
-};
-var TotalAmt;
-if(Noofdays<RentDiscountperdays["days"][0])
-{
- TotalAmt=(Noofdays*parseInt(DialyCarRent.slice(1)))
-}
-else if(Noofdays>=RentDiscountperdays["days"][0] && Noofdays<RentDiscountperdays["days"][1])
-{
- TotalAmt=((Noofdays*parseInt(DialyCarRent.slice(1)))-parseInt(RentDiscountperdays["discount"][0].slice(1)))
-}
-else
-{
- TotalAmt=((Noofdays*parseInt(DialyCarRent.slice(1)))-parseInt(RentDiscountperdays["discount"][1].slice(1)))
-}
+    var dialyCarRent = 40,
+        totalAmt, rentPerdays, rentDiscountperdays = {
+            days: [3, 7],
+            discount: [20, 50]
+        };
+    rentPerdays = (Noofdays * dialyCarRent);
+    if (Noofdays < rentDiscountperdays["days"][0]) {
+        totalAmt = rentPerdays
+    } else if (Noofdays >= rentDiscountperdays["days"][0] && Noofdays < rentDiscountperdays["days"][1]) {
+        totalAmt = (rentPerdays - (rentDiscountperdays["discount"][0]))
+    } else {
+        totalAmt = (rentPerdays - (rentDiscountperdays["discount"][1]))
+    }
 
-return TotalAmt
-  
+    return totalAmt
+
 }
