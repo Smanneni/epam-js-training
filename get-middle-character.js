1c10aa9323d341bfefda902fd6372b0b46540c69
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-04-26 09:51:45 +0530

    updated with the review comments

--- get-middle-character.js
+++ get-middle-character.js
@@ -1,13 +1,10 @@
-
 function getMiddle(inputstring) {
-    if((inputstring.length)%2==0)
-    {
-    return inputstring.charAt((inputstring.length / 2)-1)+inputstring.charAt((inputstring.length / 2))
+    Var middleChar;
+    if ((inputstring.length) % 2 == 0) {
+        middleChar = inputstring.charAt((inputstring.length / 2) - 1) + inputstring.charAt((inputstring.length / 2))
+    } else {
+        middleChar = inputstring.charAt(Math.floor(inputstring.length / 2))
     }
-    else
-    {
-     return inputstring.charAt(Math.floor(inputstring.length / 2))
-    }
-    
-   
+
+    return middleChar;
 }
