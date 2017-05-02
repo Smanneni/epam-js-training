29b5c4b7e76ba7c7a9ec3d67218978f7b5623b18
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-05-02 15:37:31 +0530

    Update rental-car-cost.js

--- Home-Work-1/rental-car-cost.js
+++ Home-Work-1/rental-car-cost.js
@@ -1,16 +1,16 @@
 function rentalCarCost(Noofdays) {
-    var dialyCarRent = 40,
-        totalAmt, rentPerdays, rentDiscountperdays = {
+    var DIALYCAR_RENT = 40,
+        totalAmt, rentPerDays, rentDiscountperdays = {
             days: [3, 7],
             discount: [20, 50]
         };
-    rentPerdays = (Noofdays * dialyCarRent);
+    rentPerDays = (Noofdays * DIALYCAR_RENT);
     if (Noofdays < rentDiscountperdays["days"][0]) {
-        totalAmt = rentPerdays
+        totalAmt = rentPerDays
     } else if (Noofdays >= rentDiscountperdays["days"][0] && Noofdays < rentDiscountperdays["days"][1]) {
-        totalAmt = (rentPerdays - (rentDiscountperdays["discount"][0]))
+        totalAmt = (rentPerDays - (rentDiscountperdays["discount"][0]))
     } else {
-        totalAmt = (rentPerdays - (rentDiscountperdays["discount"][1]))
+        totalAmt = (rentPerDays - (rentDiscountperdays["discount"][1]))
     }
 
     return totalAmt
