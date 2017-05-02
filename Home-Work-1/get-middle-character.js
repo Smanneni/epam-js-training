d811bdf4bc265b5087286c7237513a50b31338f7
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-05-02 15:58:15 +0530

    Update get-middle-character.js

--- Home-Work-1/get-middle-character.js
+++ Home-Work-1/get-middle-character.js
@@ -1,7 +1,7 @@
 function getMiddle(inputString) {
-    var middleString, stringLength = (inputString.length),
-        midleChar = (stringLength / 2);
-
+    var middleString, stringLength, midleChar;
+    stringLength = (inputString.length);
+    midleChar = (stringLength / 2);
     if (stringLength % 2 == 0) {
         middleString = inputString.charAt(midleChar - 1) + inputString.charAt(midleChar)
     } else {
