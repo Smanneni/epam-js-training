5d95bbba61e7f3b63cd6e26c2d6fc575866d681b
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-05-06 12:05:44 +0530

    updated with review comments

--- Home-Work-2/Functionwithinfunction.js
+++ Home-Work-2/Functionwithinfunction.js
@@ -9,19 +9,4 @@ function always(n) {
 var s = always(true);
 s();
 
-function always(n) {
 
-    function b() {
-        return n;
-    }
-
-    return b();
-}
-var s = always(true);
-s;
-
-function magic(x) {
-    return function calc(x) {
-        return x;
-    };
-}
