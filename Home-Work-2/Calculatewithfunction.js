c2561f012aec177a051ce9dafda34b87a0c77108
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-05-06 12:24:18 +0530

    updated with review comments

--- Home-Work-2/Calculatewithfunction.js
+++ Home-Work-2/Calculatewithfunction.js
@@ -1,68 +1,39 @@
-function zero(operator) {
-    return operator ? operator(0) : 0;
-}
-
-function one(operator) {
-    return operator ? operator(1) : 1;
-}
-
-function two(operator) {
-    return operator ? operator(2) : 2;
-}
-
-function three(operator) {
-    return operator ? operator(3) : 3;
-}
-
-function four(operator) {
-    return operator ? operator(4) : 4;
-}
-
-function five(operator) {
-    return operator ? operator(5) : 5;
-}
-
-function six(operator) {
-    return operator ? operator(6) : 6;
-}
-
-function seven(operator) {
-
-    return operator ? operator(7) : 7;
-}
-
-function eight(operator) {
-    return operator ? operator(8) : 8;
-}
-
-function nine(operator) {
-    return operator ? operator(9) : 9;
-}
-
-function plus(rightVal) {
-    return function(leftVal) {
-        return leftVal + rightVal;
+var number = function(digit) {
+    return function(operator) {
+        return operator ? operator(digit) : digit;
     }
+};
+var zero = number(0);
+var one = number(1);
+var two = number(2);
+var three = number(3);
+var four = number(4);
+var five = number(5);
+var six = number(6);
+var seven = number(7);
+var eight = number(8);
+var nine = number(9);
+
+function plus(rightval) {
+    return function(leftval) {
+        return leftval + rightval;
+    };
 }
 
-function minus(rightVal) {
-    return function(leftVal) {
-        return leftVal - rightVal;
-    }
+function minus(rightval) {
+    return function(leftval) {
+        return leftval - rightval;
+    };
 }
 
-function times(rightVal) {
-
-    return function(leftVal) {
-        console.log(leftVal);
-        return leftVal * rightVal;
-
-    }
-
+function times(rightval) {
+    return function(leftval) {
+        return leftval * rightval;
+    };
 }
 
-function dividedBy(rightVal) {
-    return function(leftVal) {
-        return leftVal / rightVal;
-    }
+function dividedBy(rightval) {
+    return function(leftval) {
+        return leftval / rightval;
+    };
 }
