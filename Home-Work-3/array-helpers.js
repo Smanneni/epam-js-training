268c2faa25e4266cbd9c3221fcdab346d9041e2e
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-05-06 12:37:04 +0530

    updated with review comments

--- Home-Work-3/array-helpers.js
+++ Home-Work-3/array-helpers.js
@@ -1,54 +1,40 @@
-Array.prototype.square = function() {
-    var retVal = [];
-    for (var i = 0, len = this.length; i < len; ++i) {
-        retVal.push(this[i] * this[i]);
-    }
-    return retVal;
-};
+Array.prototype.square = Square;
+Array.prototype.cube = Cube;
+Array.prototype.average = Average;
+Array.prototype.sum = Sum;
+Array.prototype.even = Even;
+Array.prototype.odd = Odd;
 
-Array.prototype.cube = function() {
-    var retVal = [];
-    for (var i = 0, len = this.length; i < len; ++i) {
-        retVal.push(this[i] * this[i] * this[i]);
-    }
-    return retVal;
-};
+function Square() {
+    return this.map(function(num) {
+        return num * num;
+    });
+}
 
-Array.prototype.average = function() {
-    var retVal = NaN;
-    for (var i = 0, len = this.length; i < len; ++i) {
-        if (isNaN(retval)) {
-            retVal = 0;
-        }
-        retVal += this[i];
-    }
-    return retVal / this.length;
-};
+function Cube() {
+    return this.map(function(num) {
+        return num * num * num;
+    });
+}
 
-Array.prototype.sum = function() {
-    var retVal = 0;
-    for (var i = 0, len = this.length; i < len; ++i) {
-        retVal += this[i];
-    }
-    return retVal;
-};
+function Average() {
+    return this.sum() / this.length;
+}
 
-Array.prototype.even = function() {
-    var retVal = [];
-    for (var i = 0, len = this.length; i < len; ++i) {
-        if (this[i] % 2 == 0) {
-            retVal.push(this[i]);
-        }
-    }
-    return retVal;
-};
+function Sum() {
+    return (this == '') ? 0 : this.reduce(function(a, b) {
+        return a + b;
+    });
+}
 
-Array.prototype.odd = function() {
-    var retVal = [];
-    for (var i = 0, len = this.length; i < len; ++i) {
-        if (this[i] % 2 == 1) {
-            retVal.push(this[i]);
-        }
-    }
-    return retVal;
-};
+function Even() {
+    return this.filter(function(num) {
+        return num % 2 == 0;
+    });
+}
+
+function Odd() {
+    return this.filter(function(num) {
+        return num % 2 != 0;
+    });
+}
