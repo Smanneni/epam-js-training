0911806c12a10842e11ff4f8ae356ec094cb65ac
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-05-08 12:46:50 +0530

    Update using-closures-to-share-class-state.js

--- Home-Work-3/using-closures-to-share-class-state.js
+++ Home-Work-3/using-closures-to-share-class-state.js
@@ -1,1 +1,27 @@
+var Cat = (function() {
 
+    var totalWeight = 0;
+    var countInstances = 0;
+
+    function Cat(name, weight) {
+        this.name = name;
+        Object.defineProperty(this, 'weight', {
+            get: function() {
+                return weight;
+            },
+            set: function(w) {
+                totalWeight -= weight - w;
+                weight = w;
+            }
+        });
+        totalWeight += weight;
+        countInstances += 1;
+    }
+
+    Cat.averageWeight = function() {
+        return totalWeight / countInstances;
+    };
+
+    return Cat;
+
+}());
