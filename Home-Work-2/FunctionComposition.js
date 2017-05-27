d999cbc3770895bd4661c00a15101e4462a16c3a
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-05-27 11:24:12 +0530

    implemented the review comments

--- Home-Work-2/FunctionComposition.js
+++ Home-Work-2/FunctionComposition.js
@@ -1,7 +1,7 @@
 function compose(f, g) {
     return function(step, ...rest) {
-        var g1 = g(step, ...rest);
-        var f1 = f(g1);
+        var g1 = g(step, ...rest),
+            f1 = f(g1);
         return f1;
     };
 }
