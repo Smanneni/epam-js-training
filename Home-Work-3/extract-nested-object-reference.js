6d079abe16f0bfeb52304091a051b3e4c5eb14d3
Manneni Sivakumar <smanneni@allianceglobalservices.com>
2017-05-29 19:22:17 +0530

    implemented the review comments.

--- Home-Work-3/extract-nested-object-reference.js
+++ Home-Work-3/extract-nested-object-reference.js
@@ -1,8 +1,8 @@
 // return the nested property value if it exists,
 // otherwise return undefined
 Object.prototype.hash = function(string) {
-    var current = this;
-    var property = string.split('.');
+    var current = this,
+        property = string.split('.');
 
     while (current && property.length) {
         current = current[property.shift()];
